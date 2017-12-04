29e202eebc87cf0e142644dfa308e22ead7a1717
KrisGray <kris@ebi.ac.uk>
2017-12-04 14:40:01 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+yum -y update && yum clean all;
+yum -y install wget
+yum -y install tar
+yum -y install gcc
+yum -y install zlib-devel
+yum -y install sudo
+yum -y install git
+
+service iptables stop;
+chkconfig iptables off;
+rm -rf /etc/rc.d/init.d/iptables
+
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -ivh epel-release-6-8.noarch.rpm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+npm install -g @angular/cli
